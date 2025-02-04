727ae21dc6c73079d3992e18b833337f5ce4040c
loay eslam <loayeslam5756@gmail.com>
2025-02-04 19:43:52 +0200

    Qualifier

--- src/main/java/com/spring/annotation/Master.java
+++ src/main/java/com/spring/annotation/Master.java
@@ -1,19 +1,17 @@
 package com.spring.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("master")
 public class Master implements MasterInterface {
-	
+	@Autowired
+	@Qualifier("user")
 	private CompanyInterface ci;
 	
 	
-   @Autowired
-	public Master(CompanyInterface ci) {
-	
-		this.ci = ci;
-	}
+   
 
 	@Override
 	public void name() {
